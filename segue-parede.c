3b9263f43620e28179b25a4a90e6c20a76249b1b
Willian Massahiro Hayashida <ra188705@saramago.lab.ic.unicamp.br>
2017-11-22 14:21:58 -0200

    Segue-Parede

--- segue-parede.c
+++ segue-parede.c
@@ -1,31 +1,59 @@
 #include "api_robot2.h"
 
 void main() {
-	// unsigned char i;
-	// int x;
-	// unsigned short distance;
-	//
-	// motor_cfg_t m0, m1;
-	// m0.id = 0;
-	// m1.id = 1;
-	//
-	// m0.speed = 15;
-	// m1.speed = 15;
-	// set_motors_speed(&m0, &m1);
-	// while (1) {
-	// 	distance = read_sonar(3);
-	// 	if (distance < 1200) {
-	// 		m0.speed = 0;
-	// 		m1.speed = 0;
-	// 		set_motor_speed(&m0);
-	// 		set_motor_speed(&m1);
-	// 		break;
-	// 	}
-	// }
-	//
-	// while (1);
+	unsigned char i;
+	int x;
+	unsigned short distance;
+	unsigned short distance_aux;
+	motor_cfg_t m0, m1;
+	m0.id = 0;
+	m1.id = 1;
+	
+	m0.speed = 20;
+	m1.speed = 20;
+	set_motors_speed(&m0, &m1);
+	
+	// *****************************************************************
+	// MODO BUSCAR-PAREDE
+	// *****************************************************************
+	
+	// procura parede
+	while (1) {
+		distance = read_sonar(3);
+		distance_aux = read_sonar(4);
+	 	if (distance < 950 && distance_aux < 950) {
+	 		m0.speed = 0;
+	 		m1.speed = 0;
+	 		set_motor_speed(&m0);
+	 		set_motor_speed(&m1);
+	 		break;	
+	 	}
+	}
+	
+	// vira a esquerda
+	m1.speed = 5;
+	set_motor_speed(&m1);
+	while(1) {
+		distance = read_sonar(0);
+		distance_aux = read_sonar(15);
+		
+		if (distance < distance_aux + 7 && distance > distance_aux - 7){
+			m1.speed = 0;
+			set_motor_speed(&m1);
+			break;
+		}
+	}
+	
+	// *****************************************************************
+	// MODO SEGUE-PAREDE
+	// *****************************************************************
+	
+	
+	
+	while (1);
 	int a = read_sonar(3);
 	int b = read_sonar(3);
 	int c = read_sonar(3);
 	while (1);
 }
+
